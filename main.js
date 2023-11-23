aaac3cc5de3ea84661a081dce4607158183ea18b
christinetofft <christinetofft17@gmail.com>
2023-11-23 17:05:26 +0100

    init

---
+++ main.js
@@ -0,0 +1,42 @@
+const myApiKey = "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";
+const button = document.querySelector('button');
+const moodInput = document.querySelector('#mood');
+const pTag = document.querySelector('.container .playlist');
+const loading = document.querySelector('.loader');
+const apiKeyInput = document.querySelector('#api-key')
+
+
+button.addEventListener("click", () => {
+    loading.classList.remove('none');
+    loading.classList.add('block');
+
+    const apiKey = apiKeyInput.value
+
+    fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`
+        },
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: [{
+                role: 'user',
+                content: `Follow my instructions clearly. Create a playlist of only 20 different songs with this mood: ${moodInput.value}` +
+                    'Very important that there is no repeating of songs and that the songs fits the mood. Show every song only one time! Maximum of 20 songs in the playlist.' +
+                    'Only respond with "Here is a playlist based on your mood:" and then the list of the songs. End the response with "Enjoy your new playlist!"'
+            }]
+        })
+    })
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data.choices[0].message.content)
+            pTag.innerText = data.choices[0].message.content;
+            loading.classList.remove('block');
+            loading.classList.add('none');
+    })
+})
+
+
+
+
